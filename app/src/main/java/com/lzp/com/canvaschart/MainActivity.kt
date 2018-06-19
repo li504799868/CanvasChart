e8f765550783dcbe8e2865e0c21612835062d029
lizhipeng <lizhipeng@ugirls.com>
2018-06-19 14:53:23 +0800

    解决部分问题

--- app/src/main/java/com/lzp/com/canvaschart/MainActivity.kt
+++ app/src/main/java/com/lzp/com/canvaschart/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import com.lzp.com.canvaschart.base.BaseDataAdapter
 import com.lzp.com.canvaschart.base.ChartBean
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
